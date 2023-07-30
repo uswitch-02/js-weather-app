b60ccaa6781ab52e7f8ccf4d2c4132cb590fc3c5
uswitch-02 <chunxidubian64@gmail.com>
2023-07-30 15:26:52 +0000

    [update]

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("test").innerHTML="JavaScriptテスト"
+// documentとはブラウザに表示されているページ全体のことを指す
+// この中からid属性がtestであるものを探すのが、getElementById("test")
+// get「捕まえる」Element「要素（＝HTMLタグ）」、「Byld」が「Idを使って」
+// innerHTMLがHTMLタグの中身を置き換える働きがあります。
+// ※JavaScriptのコードは「.」でつないでいく
+console.log (document.getElementById("test"))
