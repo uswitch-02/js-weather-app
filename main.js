f379b250ba93b242f8ee9ead161af7565fe6d61f
uswitch-02 <chunxidubian64@gmail.com>
2023-07-31 14:31:51 +0000

    [update]

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const weatherForm = document.getElementById("weather-form")
 const cityInput = document.getElementById("city-input")
 const weatherResults = document.getElementById("weather-results")
 
+fetch()
