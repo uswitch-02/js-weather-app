d8e40639e75a7316a49a36f447cf652f44fa9693
uswitch-02 <chunxidubian64@gmail.com>
2023-08-01 14:14:49 +0000

    [update]

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const weatherForm = document.getElementById("weather-form")
 const cityInput = document.getElementById("city-input")
 const weatherResults = document.getElementById("weather-results")
 
+const getWeather = () => {
 fetch("https://api.weatherapi.com/v1/current.json?key=210b8e4818a4439694091142232207 &q=London&aqi=no")
     .then(response => response.json())
     .then(jsonData => weatherResults.innerHTML=`
@@ -14,3 +15,4 @@ fetch("https://api.weatherapi.com/v1/current.json?key=210b8e4818a443969409114223
                 </div>
                 `
     )
+}
