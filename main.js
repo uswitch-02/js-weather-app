785b07c472392092e43a182fe908603930f02d04
uswitch-02 <chunxidubian64@gmail.com>
2023-08-03 01:37:55 +0000

    [finish]

--- main.js
+++ main.js
@@ -9,18 +9,19 @@ weatherForm.addEventListener("submit",(e)=>{
 })
 
 const getWeather = (city) => {
-fetch(`https://api.weatherapi.com/v1/current.json?key=210b8e4818a4439694091142232207 &q=${city}&aqi=no`)
-    .then(response => response.json())
-    .then(jsonData => weatherResults.innerHTML=`
-                <div class="results-country">${jsonData.location.country}</div>
-                <div class="results-city">${josnDate.location.name}</div>
-                <div class="results-temp">${jsonData.current.temp_c}</div>
-                <div class="results-condition">
-                  <img src="" alt="icon">
-                  <span>${jsonData.current.condition.text}</span>
-                </div>
-                `
-    )
+  weatherResults.innerHTML = `<div class="lading"></div>`
+  fetch(`https://api.weatherapi.com/v1/current.json?key=210b8e4818a4439694091142232207 &q=${city}&aqi=no`)
+      .then(response => response.json())
+      .then(jsonData => weatherResults.innerHTML=`
+                  <div class="results-country">${jsonData.location.country}</div>
+                  <div class="results-city">${josnDate.location.name}</div>
+                  <div class="results-temp">${jsonData.current.temp_c}</div>
+                  <div class="results-condition">
+                    <img src="" alt="icon">
+                    <span>${jsonData.current.condition.text}</span>
+                  </div>
+                  `
+      ).catch(err =>alert("エラーが発生しました。ページをリロードして、もう一度トライしてください"))
 }
 
 
